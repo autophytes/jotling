6b931e71ca372558a4e2019ba88988466262dcdd
Josh Marbert <54632192+jmarbert@users.noreply.github.com>
2020-05-25 11:26:11 -0400

    Electron boilerplate

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+// Import dependencies
+import React from 'react';
+
+// Create main App component
+const App = () => (
+	<div>
+		<h1>Hello, electron!</h1>
+
+		<p>Let's start building your awesome desktop app with electron and React!</p>
+	</div>
+);
+
+// Export the App component
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+// Import dependencies
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import main App component
+import App from './components/app';
+
+// Import CSS stylesheet
+import './assets/css/app.css';
+
+// Since we are using HtmlWebpackPlugin WITHOUT a template,
+// we should create our own root node in the body element before rendering into it
+let root = document.createElement('div');
+
+// Append root div to body
+root.id = 'root';
+document.body.appendChild(root);
+
+// Render the app into the root div
+render(<App />, document.getElementById('root'));
