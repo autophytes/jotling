33b4c893ff96e0f3400506a47a3732306750b434
Josh Marbert <54632192+jmarbert@users.noreply.github.com>
2020-09-21 14:03:49 -0400

    Dragging the peek window

--- src/components/editor/decorators/PeekDocument.jsx
+++ src/components/editor/decorators/PeekDocument.jsx
@@ -4,6 +4,8 @@ import { LeftNavContext } from '../../../contexts/leftNavContext';
 
 import { generatePeekDecorator } from './PeekDocumentDecorators';
 
+import CloseSVG from '../../../assets/svg/CloseSVG';
+
 import { Editor, EditorState } from 'draft-js';
 
 const PeekWindow = () => {
@@ -13,9 +15,62 @@ const PeekWindow = () => {
 	const [containerLeft, setContainerLeft] = useState(200);
 	const [hasScrolled, setHasScrolled] = useState(false);
 
+	const peekWindowContentRef = useRef(null);
 	const peekWindowRef = useRef(null);
 
-	const { editorArchives, linkStructureRef, peekWindowLinkId } = useContext(LeftNavContext);
+	const {
+		editorArchives,
+		linkStructureRef,
+		peekWindowLinkId,
+		setPeekWindowLinkId,
+		navData,
+		setNavData,
+	} = useContext(LeftNavContext);
+
+	// Reposition the peek window when dragging the title bar
+	const handleResizeMouseDown = (e) => {
+		const mouseX = e.clientX;
+		const mouseY = e.clientY;
+		console.log('mouse resizing');
+		// setIsResizing(true);
+		// let rootSize = Number(
+		// 	window
+		// 		.getComputedStyle(document.querySelector(':root'))
+		// 		.getPropertyValue('font-size')
+		// 		.replace('px', '')
+		// );
+
+		// let minWidth = 7 * rootSize;
+		// let maxWidth = 25 * rootSize;
+		// let widthOffset = rootSize / 4;
+
+		// When the mouse moves, update the width to reflext the mouse's X coordinate
+		const handleResizeMouseMove = (e) => {
+			if (e.clientX !== 0) {
+				const newX = containerLeft + (e.clientX - mouseX);
+				const newY = Math.max(containerTop + (e.clientY - mouseY), 0);
+
+				peekWindowRef.current.style.top = newY + 'px';
+				peekWindowRef.current.style.left = newX + 'px';
+			}
+		};
+
+		// When finshed resizing, set the width in REMs so it responds to root size changes
+		const handleResizeMouseUp = (e) => {
+			// setIsResizing(false);
+			window.removeEventListener('mousemove', handleResizeMouseMove);
+			window.removeEventListener('mouseup', handleResizeMouseUp);
+
+			const newX = containerLeft + (e.clientX - mouseX);
+			const newY = Math.max(containerTop + (e.clientY - mouseY), 0);
+
+			setContainerTop(300);
+			setContainerLeft(300);
+		};
+
+		window.addEventListener('mousemove', handleResizeMouseMove);
+		window.addEventListener('mouseup', handleResizeMouseUp);
+	};
 
 	useEffect(() => {
 		setDocumentName(linkStructureRef.current.links[peekWindowLinkId].source);
@@ -43,9 +98,9 @@ const PeekWindow = () => {
 		if (!hasScrolled && firstPeekHighlightElement) {
 			const highlightRect = firstPeekHighlightElement.getBoundingClientRect();
 
-			const windowRect = peekWindowRef.current.getBoundingClientRect();
+			const windowRect = peekWindowContentRef.current.getBoundingClientRect();
 
-			peekWindowRef.current.scrollTo({
+			peekWindowContentRef.current.scrollTo({
 				top: highlightRect.top - windowRect.top - 100,
 				behavior: 'smooth',
 			});
@@ -54,12 +109,30 @@ const PeekWindow = () => {
 	}, [editorState, hasScrolled]);
 
 	return (
-		<div
-			className='peek-window'
-			ref={peekWindowRef}
-			style={{ top: `${containerTop}px`, left: `${containerLeft}px` }}>
-			<Editor editorState={editorState} readOnly />
-		</div>
+		<>
+			<div
+				className='peek-window'
+				ref={peekWindowRef}
+				style={{ top: `${containerTop}px`, left: `${containerLeft}px` }}>
+				<div className='peek-window-top-handle' onMouseDown={handleResizeMouseDown}>
+					<button
+						className='peek-window-open'
+						onClick={() => {
+							setNavData({ ...navData, currentDoc: documentName });
+							setPeekWindowLinkId(null);
+						}}>
+						Open Document
+					</button>
+					<p className='peek-window-title'>{documentName}</p>
+					<button className='peek-window-close' onClick={() => setPeekWindowLinkId(null)}>
+						<CloseSVG />
+					</button>
+				</div>
+				<div className='peek-window-contents' ref={peekWindowContentRef}>
+					<Editor editorState={editorState} readOnly />
+				</div>
+			</div>
+		</>
 	);
 };
 
